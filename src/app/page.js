288f8e2a46fc3a7556764b512b3ca2e0ecce7588
‪Hani Abdelkareem <hanim1033@gmail.com>
2024-09-26 13:17:06 +0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <PageHeader
-        h1Text={"Add Epic Captions to Your Videos"}
+        h1Text={"Add Amazing Captions to Your Videos"}
         h2Text={"Just upload your video and we will do the rest"}
       />
       <div className="flex justify-center">
